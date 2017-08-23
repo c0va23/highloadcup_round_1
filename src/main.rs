3f3ee720958e2a6f3f1d66d0c167a7130db96a8f
Dmitrij Fedorenko <c0va23@gmail.com>
2017-08-23 10:04:07 +0300

    Configure base hyper server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate futures;
+extern crate hyper;
+
+use std::env;
+
+use hyper::server;
+use futures::{
+    Future,
+    future,
+};
+
+
+struct Router;
+
+impl server::Service for Router {
+    type Request = server::Request;
+    type Response = server::Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, _req: Self::Request) -> Self::Future {
+        future::ok(server::Response::new().with_body("Hello!")).boxed()
+    }
+}
+
+const DEFAULT_LISTEN: &'static str = "127.0.0.1:9999";
+
 fn main() {
-    println!("Hello, world!");
+    let address = env::var("LISTEN").unwrap_or(DEFAULT_LISTEN.to_string())
+        .parse().unwrap();
+    hyper::server::Http::new()
+        .bind(&address, || Ok(Router)).unwrap()
+        .run().unwrap()
 }
