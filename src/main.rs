a093e649c91d4e35ceee35f17df78c4819d59331
Dmitrij Fedorenko <c0va23@gmail.com>
2017-08-24 09:41:11 +0300

    Add GET /users/:id

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,56 @@ struct Router<'a> {
     store: &'a store::Store,
 }
 
+struct PathParts<'a> {
+    entity: &'a str,
+    id: Option<u32>,
+    action: Option<&'a str>,
+}
+
 impl<'a> Router<'a> {
     fn new(store: &'a store::Store) -> Self {
         Self {
             store: store,
+        }
+    }
+
+    fn parse_path(path: &str) -> Result<PathParts, ()> {
+        let parts = path.split("/");
+        if 2 <= parts.clone().count() && parts.clone().count() <= 3 {
+            match parts.clone().nth(1).unwrap().parse() {
+                Ok(id) => Ok(PathParts{
+                    entity: parts.clone().nth(0).unwrap(),
+                    id: Some(id),
+                    action: parts.clone().nth(2),
+                }),
+                Err(_) => Ok(PathParts {
+                    entity: parts.clone().nth(0).unwrap(),
+                    id: None,
+                    action: parts.clone().nth(1),
+                }),
+            }
+        } else {
+            return Err(())
+        }
+    }
+
+    fn not_found() -> Box<Future<Item = server::Response, Error = hyper::Error>> {
+        future::ok(server::Response::new().with_status(hyper::StatusCode::NotFound)).boxed()
+    }
+
+    fn internal_error() -> Box<Future<Item = server::Response, Error = hyper::Error>> {
+        future::ok(server::Response::new().with_status(hyper::StatusCode::InternalServerError)).boxed()
+    }
+
+    fn get_user(&self, id: u32) -> Box<Future<Item = server::Response, Error = hyper::Error>> {
+        match self.store.get_user(id) {
+            Ok(user) =>
+                match serde_json::to_string(&user) {
+                    Ok(json) => future::ok(server::Response::new().with_body(json.to_string())).boxed(),
+                    Err(_) => Self::internal_error(),
+                },
+            Err(store::StoreError::EntryExists) => Self::not_found(),
+            Err(_) => Self::internal_error(),
         }
     }
 }
@@ -36,7 +82,11 @@ impl<'a> server::Service for Router<'a> {
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        future::ok(server::Response::new().with_status(hyper::StatusCode::NotFound)).boxed()
+        match (req.method(), Self::parse_path(req.path())) {
+            (&hyper::Method::Get, Ok(PathParts{ action: None, id: Some(id), entity: "users" })) =>
+                self.get_user(id),
+            _ => Self::not_found(),
+        }
     }
 }
 
